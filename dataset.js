76abb6e30e51e52b3d64d282d1f340b806c0ce43
harshaljain07 <40124915+harshaljain07@users.noreply.github.com>
2021-10-13 15:55:28 +0530

    Create dataset.js

---
+++ dataset.js
@@ -0,0 +1,45 @@
+//Data sets to test algorithm-
+
+//testcase 2
+const accountBalanceHistory = [
+    {
+        monthNumber: 1, // current month
+        account: {
+            balance: { amount: 200 },
+        },
+    },
+    {
+        monthNumber: 0, // last month
+        account: {
+            balance: { amount: 100 },
+        },
+    },
+    {
+        monthNumber: 2, // two months ago
+        account: {
+            balance: { amount: 300 },
+        },
+    }
+]
+
+//testcase 3
+const accountBalanceHistory = [
+    {
+        monthNumber: 0, // current month
+        account: {
+            balance: { amount: 100 },
+        },
+    },
+    {
+        monthNumber: 1, // last month
+        account: {
+            balance: { amount: 10 },
+        },
+    },
+    {
+        monthNumber: 2, // two months ago
+        account: {
+            balance: { amount: 200 },
+        },
+    }
+]
